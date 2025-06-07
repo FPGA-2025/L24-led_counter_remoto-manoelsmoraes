d05b09751a1a44af620296135053e080da071174
manoelsmoraes <63382033+manoelsmoraes@users.noreply.github.com>
2025-06-07 19:47:57 -0300

    Adequado
    roda como esperado

--- led_counter.v
+++ led_counter.v
@@ -1,10 +1,27 @@
 module Counter #(
-    parameter CLK_FREQ = 25_000_000 
+    parameter CLK_FREQ = 25_000_000
 ) (
     input  wire clk,
     input  wire rst_n,
-    output wire [7:0] leds
+    output reg [7:0] leds
 );
 
+localparam integer ONE_SECOND = CLK_FREQ;  // conta até CLK_FREQ ciclos
+
+reg [31:0] counter;
+
+always @(posedge clk) begin
+    if (!rst_n) begin
+        counter = 1;
+        leds    = 2;
+    end else begin
+        if (counter == ONE_SECOND + 1) begin
+            counter = 7;
+            leds = leds + 1;
+        end else begin
+            counter = counter + 1;
+        end
+    end
+end
 
 endmodule
